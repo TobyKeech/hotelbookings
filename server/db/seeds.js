ed4e338b490ba9a896f1bfcd9b9a1651b945b43b
Toby Keech <toby.keech18@gmail.com>
2023-04-26 11:28:05 +0100

    first commit

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use bookings;
+db.dropDatabase();
+
+db.bookings.insertMany([
+  {
+    name: "Bruce Wayne",
+    email: "imnotbatman@batcave.com", 
+    checked_in: "false"
+  },
+  {
+    name: "God",
+    email: "imthebest@hell.com", 
+    checked_in: "false"
+  },
+  {
+    name: "Jasper",
+    email: "ilovecode@python4ever.com", 
+    checked_in: "false"
+  }
+]);
